43eeac2feb6f0c69257cc58e6ba6b7f28f33ccbd
Aleksandr Zhitenyov <aleksandr.zhitenyov@altenar.com>
2026-02-12 23:04:38 +0300

    add getter in to response model

--- src/test/java/model/Response.java
+++ src/test/java/model/Response.java
@@ -6,6 +6,25 @@ public class Response {
     public String subject;
     public Data data;
 
+    public Response() {
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
     class Data {
         public String bestAsk;
         public String bestAskSize;
@@ -15,5 +34,40 @@ public class Response {
         public String sequence;
         public String size;
         public long time;
+
+        public Data() {
+        }
+
+        public String getBestAsk() {
+            return bestAsk;
+        }
+
+        public String getBestAskSize() {
+            return bestAskSize;
+        }
+
+        public String getBestBid() {
+            return bestBid;
+        }
+
+        public String getBestBidSize() {
+            return bestBidSize;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public String getSequence() {
+            return sequence;
+        }
+
+        public String getSize() {
+            return size;
+        }
+
+        public long getTime() {
+            return time;
+        }
     }
 }
