55f5662561cfb6261baabad85c9447f0c1539e00
Aleksandr Zhitenyov <aleksandr.zhitenyov@altenar.com>
2026-02-12 22:59:27 +0300

    add response model

---
+++ src/test/java/model/Response.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Response {
+    public String topic;
+    public String type;
+    public String subject;
+    public Data data;
+
+    static class Data {
+        public String bestAsk;
+        public String bestAskSize;
+        public String bestBid;
+        public String bestBidSize;
+        public String price;
+        public String sequence;
+        public String size;
+        public long time;
+    }
+}
